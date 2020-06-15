07b042eaa64193baa7f7fb37dbb708fe752eb6c3
Karan Verma <karanv598@gmail.com>
2020-06-15 19:06:25 +0530

    Creating ObjectID

--- mongodb.js
+++ mongodb.js
@@ -1,12 +1,21 @@
 // CRUD - Create, Read, Update and Delete
 
-const mongodb = require('mongodb')
+// const mongodb = require('mongodb')
 
-// To initialise the client
-const MongoClient = mongodb.MongoClient
+// // To initialise the client
+// const MongoClient = mongodb.MongoClient
+
+// //Creating own objectID
+// const ObjectId = mongodb.ObjectID
+
+const { MongoClient, ObjectId, ObjectID } = require('mongodb')
 
 const connectionURL = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
+
+const id = new ObjectID() // To generate a new ID
+console.log(id)
+console.log(id.getTimestamp())
 
 MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client) => {
     if(error) {
@@ -17,16 +26,17 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
 
     // Inserting a single user
 
-    // db.collection('users').insertOne({
-    //     name: 'Karan Verma',
-    //     age: 23
-    // }, (error, result) => {
-    //     if(error) {
-    //        return console.log('Unable to insert user')
-    //     }
+    db.collection('users').insertOne({
+        _id: id,
+        name: 'Damon',
+        age: 27
+    }, (error, result) => {
+        if(error) {
+           return console.log('Unable to insert user')
+        }
 
-    //     console.log(result.ops)
-    // })
+        console.log(result.ops)
+    })
 
     // Inserting multiple users
 
@@ -47,24 +57,26 @@ MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client)
     //     console.log(result.ops)
     // })
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'This is description of the task one',
-            completed: true
-        },
-        {
-            description: 'This is description of the task two',
-            completed: false
-        },
-        {
-            description: 'This is description of the task three',
-            completed: true
-        }
-    ], (error, result) => {
-        if(error) {
-            return console.log('Unable to insert the document')
-        }
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'This is description of the task one',
+    //         completed: true
+    //     },
+    //     {
+    //         description: 'This is description of the task two',
+    //         completed: false
+    //     },
+    //     {
+    //         description: 'This is description of the task three',
+    //         completed: true
+    //     }
+    // ], (error, result) => {
+    //     if(error) {
+    //         return console.log('Unable to insert the document')
+    //     }
 
-        console.log(result.ops)
-    })
+    //     console.log(result.ops)
+    // })
+
+
 })
