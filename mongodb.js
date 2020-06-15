959983e3f7de3c1afda08b0ba86808d8bf7bf8a7
Karan Verma <karanv598@gmail.com>
2020-06-15 18:27:20 +0530

    Initiating mongodb and performing insert operation

---
+++ mongodb.js
@@ -0,0 +1,22 @@
+// CRUD - Create, Read, Update and Delete
+
+const mongodb = require('mongodb')
+
+// To initialise the client
+const MongoClient = mongodb.MongoClient
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useUnifiedTopology: true }, (error, client) => {
+    if(error) {
+        return console.log('Unable to connect to database!')
+    }
+
+    const db = client.db(databaseName)
+
+    db.collection('users').insertOne({
+        name: 'Karan Verma',
+        age: 23
+    })
+})
