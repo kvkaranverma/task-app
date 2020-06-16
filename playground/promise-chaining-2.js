972ab65c3bc57425895b39fc08a110daa22f4ac2
Karan Verma <karanv598@gmail.com>
2020-06-16 22:22:36 +0530

    async/await

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -2,12 +2,26 @@ require('../src/db/mongoose')
 
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5ee87465a657460b74b2132b').then((task) => {
-    console.log('Deleted Task: ' + task)
+// Promise chaining
+// Task.findByIdAndDelete('5ee87465a657460b74b2132b').then((task) => {
+//     console.log('Deleted Task: ' + task)
 
-    return Task.countDocuments({ completed: false })
-}).then(tasks => {
-    console.log(tasks)
-}).catch(error => {
-    console.log(error)
+//     return Task.countDocuments({ completed: false })
+// }).then(tasks => {
+//     console.log(tasks)
+// }).catch(error => {
+//     console.log(error)
+// })
+
+// async/await
+deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({ completed: false })
+    return count
+}
+
+deleteTaskAndCount('5ee8ea0db00ffd0b08d9cb7c').then(count => {
+    console.log(count)
+}).catch(err => {
+    console.log(err)
 })
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -2,12 +2,25 @@ require('../src/db/mongoose')
 
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('5ee872486cf1650a00a7fe84', { age: 22 }).then(user => {
-    console.log(user)
+// User.findByIdAndUpdate('5ee872486cf1650a00a7fe84', { age: 22 }).then(user => {
+//     console.log(user)
 
-    return User.countDocuments({ age: 22 })
-}).then(count => {
+//     return User.countDocuments({ age: 22 })
+// }).then(count => {
+//     console.log(count)
+// }).catch(error => {
+//     console.log(error)
+// })
+
+// async/await
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, { age })
+    const count = await User.countDocuments({ age })
+    return count
+}
+
+updateAgeAndCount('5ee872a341205b3e644bbbbc', 22).then(count => {
     console.log(count)
-}).catch(error => {
-    console.log(error)
+}).catch(err => {
+    console.log(err)
 })
