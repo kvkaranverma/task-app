5e07057d81233b72eb94114ca06a9536fd154d6d
Karan Verma <karanv598@gmail.com>
2020-06-16 12:59:26 +0530

    Task Challenge, creating new collection and saving data

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,29 +4,54 @@ const mongoose = require('mongoose')
 const connectionURL = 'mongodb://127.0.0.1:27017/task-manager-api'
 // Setting up connection
 mongoose.connect(connectionURL, {
-    useUnifiedTopology: true,
+    useNewUrlParser: true,
     useCreateIndex: true
 })
 
 // Creating model
-const User = mongoose.model('User', {
-    name: {
+// const User = mongoose.model('User', {
+//     name: {
+//         type: String
+//     },
+//     age: {
+//         type: Number
+//     }
+// })
+
+// // Creating instance
+// const me = new User({
+//     name: 'Karan Verma',
+//     age: 23
+// })
+
+// //Saving instance to the database
+// me.save().then((me) => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('error!', error)
+// })
+
+// Challenge task
+
+//Creating model
+const Task = mongoose.model('Task', {
+    description: {
         type: String
     },
-    age: {
-        type: Number
+    completed: {
+        type: Boolean
     }
 })
 
 // Creating instance
-const me = new User({
-    name: 'Karan Verma',
-    age: 23
+const task_1 = new Task({
+    description: 'Done with node',
+    completed: true
 })
 
-//Saving instance to the database
-me.save().then((me) => {
-    console.log(me)
+// Saving it to database
+task_1.save().then((task) => {
+    console.log(task)
 }).catch((error) => {
     console.log('error!', error)
 })
