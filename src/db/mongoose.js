322a5dc015eada5eebba76c490b5694feb72899e
Karan Verma <karanv598@gmail.com>
2020-06-16 13:09:44 +0530

    Validating data

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -9,49 +9,30 @@ mongoose.connect(connectionURL, {
 })
 
 // Creating model
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// })
-
-// // Creating instance
-// const me = new User({
-//     name: 'Karan Verma',
-//     age: 23
-// })
-
-// //Saving instance to the database
-// me.save().then((me) => {
-//     console.log(me)
-// }).catch((error) => {
-//     console.log('error!', error)
-// })
-
-// Challenge task
-
-//Creating model
-const Task = mongoose.model('Task', {
-    description: {
-        type: String
+const User = mongoose.model('User', {
+    name: {
+        type: String,
+        required: true
     },
-    completed: {
-        type: Boolean
+    age: {
+        type: Number,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be greater than one')
+            }
+        }
     }
 })
 
 // Creating instance
-const task_1 = new Task({
-    description: 'Done with node',
-    completed: true
+const me = new User({
+    name: 'Elena',
+    age: -1
 })
 
-// Saving it to database
-task_1.save().then((task) => {
-    console.log(task)
+//Saving instance to the database
+me.save().then((me) => {
+    console.log(me)
 }).catch((error) => {
     console.log('error!', error)
 })
