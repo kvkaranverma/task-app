cf0bf9e48773b17fa00b7d625352cf48e200508a
Karan Verma <karanv598@gmail.com>
2020-06-16 19:50:34 +0530

    Creating tasks from Postman REST APIs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 require('./db/mongoose')
 
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,7 +13,17 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then((user) => {
-        res.send(user)
+        res.status(201).send(user)
+    }).catch((error) => {
+        res.status(400).send(error)
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then((task) => {
+        res.status(201).send(task)
     }).catch((error) => {
         res.status(400).send(error)
     })
