afb38dff27599dec2e015f42300a19805d753b92
Karan Verma <karanv598@gmail.com>
2020-06-18 22:22:14 +0530

    Auto cropping and image formatting

--- src/index.js
+++ src/index.js
@@ -8,27 +8,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-const multer = require('multer')
-const upload = multer({
-    dest: 'images',
-    limits: {
-        fileSize: 10000000
-    },
-    fileFilter(req, file, callback) {
-        if(!file.originalname.match(/\.(doc|docx)$/)) {
-            return callback(new Error('Please upload a word document'))
-        }
-
-        callback(undefined, true)
-    }
-})
-
-app.post('/upload', upload.single('upload'), (req, res) => {
-    res.send()
-}, (error, req, res, next) => {
-    res.status(400).send({ error: error.message })
-})
-
 app.use(express.json())
 app.use(userRouter, taskRouter)
 
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const multer = require('multer')
+const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 
@@ -116,7 +117,8 @@ router.delete('/user/me', auth, async (req, res) => {
 })
 
 router.post('/user/me/avatar', auth, upload.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250}).png().toBuffer()
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 }, (error, req, res, next) => {
@@ -137,7 +139,7 @@ router.get('/user/:id/avatar', async (req, res) => {
             throw new Error()
         }
 
-        res.set('Content-Type', 'image/jpg')
+        res.set('Content-Type', 'image/png')
         res.send(user.avatar)
     }
     catch(error) {
